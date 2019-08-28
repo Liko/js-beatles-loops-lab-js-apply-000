8e15f18e9c73027a40c11dfec592dc184ba728c3
Li Xie <li_xie1992@hotmail.com>
2019-08-28 11:58:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,26 @@ function theBeatlesPlay(musicians, instruments) {
   for (var i = 0; i < musicians.length; i++) {
     myArray.put(`${musicians[i]} plays ${instruments[i]}`)
   }
+  
+  return myArray
+}
+
+function johnLennonFacts(facts) {
+  count = 0
+  myArray = []
+  
+  while (count < facts.length) {
+    myArray.put(`${facts[count]}!!!`)
+    count++
+  }
+  
+  return myArray
+}
+
+function iLoveTheBeatles(num) {
+  myArray = []
+  
+  do {
+    myArray.put("I love the Beatles!")
+  }
 }
